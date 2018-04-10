4fe507ab1fc27745746750f323fb1e9316f67066
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-04-10 18:00:36 +0900

    Add heroku button

--- sig/routes/index.js
+++ sig/routes/index.js
@@ -6,7 +6,7 @@ var config = require('../config');
 /* GET home page. */
 router.post('/sign', function(req, res, next) {
   var sig = crypto
-        .createHmac("SHA256", config.clientKey)
+        .createHmac("SHA256", process.env.CLIENT_KEY || config.clientKey)
         .update(req.body.body).digest("base64");
   res.json({sig});
 });
